a8e7dfa7e15420c587345be42cd4bd513658ac8f
plaresmedima <steven.sourbron@googlemail.com>
2025-01-11 18:10:47 +0000

    added test for fetch

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,18 +1,18 @@
+import numpy as np
+
 import vreg
 import vreg.plot as plt
 
 
 def test_fetch():
-    img = vreg.fetch('Dixon_out_phase')
-    img = img.extract_slice(100)
     mask = vreg.fetch('left_kidney')
-    plt.overlay_2d(img, mask)
-
-def test_clear_cache():
-    vreg.clear_cache()
+    assert 1504 == np.sum(mask.values[:,:,2])
+    vreg.fetch(clear_cache=True)
+    vreg.fetch(download_all=True)
+    vreg.fetch(download_all=True)
 
 
 if __name__ == '__main__':
 
-    test_fetch()
-    #test_clear_cache()
+    #test_fetch()
+    vreg.fetch(clear_cache=True)
